012b2c45623771e8dda20843ae937dd60ad26a92
Manu Pranav <manupranav62@gmail.com>
2024-02-01 20:27:46 +0530

    auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("User not authorized");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("No token found");
+  }
+});
