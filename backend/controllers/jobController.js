58f0abdcbcd8a9060b1582583bec7836e06e1e61
Manu Pranav <manupranav62@gmail.com>
2024-02-01 20:27:20 +0530

    added cron schedule

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,6 +1,16 @@
 const asyncHandler = require("express-async-handler");
 const Job = require("../model/jobModel");
+const Webhook = require("../model/notificationModel");
 const scrapeJobData = require("../services/scrapeService");
+const protect = require("../middleware/authMiddleware");
+const cron = require("node-cron");
+const axios = require("axios");
+
+// Schedule the setJobs function to run every hour
+cron.schedule("0 * * * *", async () => {
+  console.log("Running setJobs cron job");
+  await setJobsInternal();
+});
 
 // @desc Get jobs
 // @route GET /api/jobs
@@ -13,7 +23,7 @@ const getJobs = asyncHandler(async (req, res) => {
 // @desc Set jobs by scraping and adding to the database
 // @route POST /api/jobs
 // @access private
-const setJobs = asyncHandler(async (req, res) => {
+const setJobsInternal = async () => {
   try {
     // Scrape job data
     const jobsFromScrape = await scrapeJobData();
@@ -46,28 +56,69 @@ const setJobs = asyncHandler(async (req, res) => {
     const jobsToInsert = uniqueJobs.filter((job) => job !== null);
 
     // Insert scraped jobs into the database
-    await Job.insertMany(jobsToInsert);
+    const insertedJobs = await Job.insertMany(jobsToInsert);
 
-    res.status(201).json({ message: "Jobs successfully added." });
+    // Notify interested users for each newly inserted job
+    insertedJobs.forEach(async (job) => {
+      const interestedWebhooks = await Webhook.find({ userId: job.user });
+
+      interestedWebhooks.forEach(async (webhook) => {
+        const notificationMessage = {
+          content: `A new job has been added: ${job.companyName} - ${job.jobRole}`,
+        };
+
+        try {
+          await axios.post(webhook.webhookURL, notificationMessage);
+        } catch (error) {
+          console.error("Error sending Discord notification:", error);
+        }
+      });
+    });
+
+    console.log("Jobs successfully added.");
   } catch (error) {
     console.error("Error adding jobs:", error);
-    res.status(500).json({ error: "Internal Server Error" });
   }
+};
+
+// @desc Set jobs route
+// @route POST /api/jobs
+// @access private
+const setJobs = asyncHandler(async (req, res) => {
+  await setJobsInternal();
+  res.status(201).json({ message: "Jobs successfully added." });
 });
 
 // @desc Update jobs by ID
 // @route PUT /api/jobs/:id
 // @access private
 const putJobs = asyncHandler(async (req, res) => {
-  // Implement update functionality based on your requirements
-  res.status(200).json({ message: "Update jobs." });
+  const { jobId, status } = req.body;
+
+  if (!status || ["applied", "not intrested", "pending"].includes(status)) {
+    return res.status(400).json({ message: "Invalid status provided." });
+  }
+  try {
+    const job = await Job.findByIdAndUpdate(
+      jobId,
+      { $set: { status } },
+      { new: true }
+    );
+
+    if (!job) {
+      return res.status(404).json({ message: "Job not found." });
+    }
+    res.status(200).json({ message: "Job status updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 // @desc Delete jobs by ID
 // @route DELETE /api/jobs/:id
 // @access private
 const deleteJobs = asyncHandler(async (req, res) => {
-  // Implement delete functionality based on your requirements
   res.status(200).json({ message: "Delete jobs." });
 });
 
