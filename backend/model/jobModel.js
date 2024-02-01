a060492e8bbe85f58b8973bd3722baa7af65e808
Manu Pranav <manupranav62@gmail.com>
2024-02-01 20:24:26 +0530

    added job model

--- backend/model/jobModel.js
+++ backend/model/jobModel.js
@@ -2,12 +2,20 @@ const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
     techPark: { type: String, required: true },
     companyName: { type: String, required: true },
     jobRole: { type: String, required: true },
-    deadline: { type: Date, required: true },
+    deadline: { type: String, required: true },
     jobLink: { type: String, required: true },
-    status: { type: Boolean, default: false },
+    status: {
+      type: Strung,
+      default: "pending",
+      enum: ["applied", "pending", "not interested"],
+    },
   },
   { timestamps: true }
 );
