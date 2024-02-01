f77353d211f6e8296c7567cdc0de9c0ae0c2cbc7
Manu Pranav <manupranav62@gmail.com>
2024-02-01 20:26:43 +0530

    user model

---
+++ backend/model/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model(User, userSchema);
