32842fb5f19fae18515e0999ea238fb8f61b00d4
Manu Pranav <manupranav62@gmail.com>
2024-02-06 21:00:50 +0530

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:8082", "http://localhost:3000"], // Update  React app's URL
+  origin: [
+    "http://localhost:8082",
+    "http://localhost:3000, https://fresherjobfinder.netlify.app",
+  ], // Update  React app's URL
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
