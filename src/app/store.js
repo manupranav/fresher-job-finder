c220429caabc6bd93baed6fc0cadc4c2edbb4c3b
Manu Pranav <manupranav62@gmail.com>
2024-02-06 19:06:59 +0530

    reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import jobReducer from "../features/jobs/jobSlice";
+import notificationReducer from "../features/notifications/notificationSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    jobs: jobReducer,
+    notifications: notificationReducer,
   },
 });
