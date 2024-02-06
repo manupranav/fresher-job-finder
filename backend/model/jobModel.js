51aa72892ecc08040bf8ba22b4e5e85208d1e36e
Manu Pranav <manupranav62@gmail.com>
2024-02-06 18:53:51 +0530

    account required

--- backend/model/jobModel.js
+++ backend/model/jobModel.js
@@ -5,6 +5,7 @@ const jobSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
     techPark: { type: String, required: true },
     companyName: { type: String, required: true },
