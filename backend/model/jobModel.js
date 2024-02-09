0ba1093abef0428a3248af6953720008ec97e62f
Manu Pranav <manupranav62@gmail.com>
2024-02-09 14:27:13 +0530

    removed user ref

--- backend/model/jobModel.js
+++ backend/model/jobModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    // user: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "User",
+    //   required: true,
+    // },
     techPark: { type: String, required: true },
     companyName: { type: String, required: true },
     jobRole: { type: String, required: true },
