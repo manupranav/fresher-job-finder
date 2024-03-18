beb57db3e866b9d745165fb076e52151c88226ba
Manu Pranav <manupranav62@gmail.com>
2024-03-18 12:29:03 +0530

    added existing user checks

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,6 +13,11 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email });
 
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
