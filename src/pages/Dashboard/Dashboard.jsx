e1d411236f6eef9e84deaea19ef74998f3fc9228
Manu Pranav <manupranav62@gmail.com>
2024-03-19 11:54:26 +0530

    changed header

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -57,114 +57,102 @@ const Dashboard = () => {
   }, [jobList, searchTerm, filter]);
 
   return (
-    <>
-      <div>
-        <section className="heading text-center">
-          <h1 className="font-semibold">
-            Welcome,{" "}
-            {user ? (
-              user.name.split(" ")[0].toLowerCase()
-            ) : (
-              <span className="text-sm lowercase text-gray-600">
-                Create an account to get instant job notifications.
-              </span>
-            )}
-          </h1>
-          <p className="text-gray-600 text-2xl">Job Dashboard</p>
-        </section>
+    <div className="mx-auto max-w-screen-lg">
+      <header className="heading text-center">
+        <h1 className="font-semibold">
+          Welcome,{" "}
+          {user ? (
+            user.name.split(" ")[0].toLowerCase()
+          ) : (
+            <span className="text-sm lowercase text-gray-600">
+              Create an account to get instant job notifications.
+            </span>
+          )}
+        </h1>
+        <p className="text-gray-700 text-2xl font-semibold mt-7">
+          JOB DASHBOARD
+        </p>
+      </header>
 
-        <div className="mx-auto max-w-screen-lg bg-white p-4 border rounded-lg shadow-lg">
-          {/* Search Bar */}
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search by company name, job role or techpark"
-            className="bg-gray-100 text-gray-700 rounded-lg block w-full px-4 py-2 mb-4"
-          />
+      <div className="bg-white border p-3 rounded-lg shadow-lg">
+        {/* Search Bar */}
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search by company name, job role, or techpark"
+          className="bg-gray-100 text-gray-700 rounded-lg block w-full px-4 py-2 mb-4"
+        />
 
-          {/* Job List */}
-          <div className="overflow-x-auto">
-            {loading ? (
-              <Spinner /> // Display your custom spinner component
-            ) : (
-              <table className="w-full text-sm text-left text-gray-700 border rounded-lg overflow-hidden">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-100">
-                  <tr>
-                    <th scope="col" className="px-6 py-3 rounded-tl-lg">
-                      Sl No
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Company Name
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Job Role
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Deadline
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Job Link
-                    </th>
-                    <th scope="col" className="px-6 py-3 rounded-tr-lg">
-                      Tech Park
-                    </th>
+        {/* Job List */}
+        <div className="overflow-x-auto">
+          {loading ? (
+            <Spinner />
+          ) : (
+            <table className="w-full text-sm text-left text-gray-700 border rounded-lg overflow-hidden">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-100">
+                <tr>
+                  <th className="px-6 py-3 rounded-tl-lg">Sl No</th>
+                  <th className="px-6 py-3">Company Name</th>
+                  <th className="px-6 py-3">Job Role</th>
+                  <th className="px-6 py-3">Deadline</th>
+                  <th className="px-6 py-3">Job Link</th>
+                  <th className="px-6 py-3 rounded-tr-lg">Tech Park</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredJobs.map((job, index) => (
+                  <tr key={job._id} className="bg-white">
+                    <td className="py-2 px-4">{index + 1}</td>
+                    <td className="py-2 px-4">{job.companyName}</td>
+                    <td className="py-2 px-4">
+                      {job.jobRole}{" "}
+                      {getToday() === job.deadline ? (
+                        <span
+                          className="new-indicator"
+                          style={{
+                            background: "red",
+                            borderRadius: "50%",
+                            display: "inline-block",
+                            width: "8px",
+                            height: "8px",
+                            marginLeft: "5px",
+                          }}
+                        ></span>
+                      ) : isJobNew(job.createdAt) ? (
+                        <span
+                          className="new-indicator"
+                          style={{
+                            background: "green",
+                            borderRadius: "50%",
+                            display: "inline-block",
+                            width: "8px",
+                            height: "8px",
+                            marginLeft: "5px",
+                          }}
+                        ></span>
+                      ) : null}
+                    </td>
+                    <td className="py-2 px-4">{job.deadline}</td>
+                    <td className="py-2 px-4">
+                      <a
+                        href={job.jobLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-700 visited:text-gray-700 hover:underline focus:underline"
+                      >
+                        View Job
+                      </a>
+                    </td>
+                    <td className="py-2 px-4">{job.techPark}</td>
                   </tr>
-                </thead>
-                <tbody>
-                  {filteredJobs.map((job, index) => (
-                    <tr className="bg-white" key={job.id}>
-                      <td className="py-2 px-4">{index + 1}</td>
-                      <td className="py-2 px-4">{job.companyName}</td>
-                      <td className="py-2 px-4">
-                        {job.jobRole}{" "}
-                        {getToday() === job.deadline ? (
-                          <span
-                            className="new-indicator"
-                            style={{
-                              background: "red",
-                              borderRadius: "50%",
-                              display: "inline-block",
-                              width: "8px",
-                              height: "8px",
-                              marginLeft: "5px",
-                            }}
-                          ></span>
-                        ) : isJobNew(job.createdAt) ? (
-                          <span
-                            className="new-indicator"
-                            style={{
-                              background: "green",
-                              borderRadius: "50%",
-                              display: "inline-block",
-                              width: "8px",
-                              height: "8px",
-                              marginLeft: "5px",
-                            }}
-                          ></span>
-                        ) : null}
-                      </td>
-                      <td className="py-2 px-4">{job.deadline}</td>
-                      <td className="py-2 px-4">
-                        <a
-                          href={job.jobLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-blue-700 visited:text-gray-700 hover:underline focus:underline"
-                        >
-                          View Job
-                        </a>
-                      </td>
-                      <td className="py-2 px-4">{job.techPark}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-          </div>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
