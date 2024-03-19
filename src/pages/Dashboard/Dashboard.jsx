844bff847440e6669bd2e46509265aefb60edb0d
Manu Pranav <manupranav62@gmail.com>
2024-03-19 09:49:40 +0530

    typo fix

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -82,7 +82,7 @@ const Dashboard = () => {
             type="text"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search by company name, job role, or techpark"
+            placeholder="Search by company name, job role or techpark"
             className="bg-gray-100 text-gray-700 rounded-lg block w-full px-4 py-2 mb-4"
           />
 
